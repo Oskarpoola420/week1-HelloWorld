945b510b8b07684579b1d66e39e778bf34c869ec
Opilane TTHK <opilane@tthk.ee>
2021-08-31 12:12:40 +0300

    add ReadLine

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,8 +6,10 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Enrike!");
-
+            Console.WriteLine("nimi on mis uhh?");
+            //ReadLine() to read the user input from the console
+            string UserName = Console.ReadLine();
+            Console.WriteLine("Hello," + UserName);
 
             Console.Read();
         }
